d8f9892b025301d34750a36f1a09d1fa699857e9
Pablo Huijse <pablo.huijse@gmail.com>
2022-03-05 18:46:05 -0300

    Replace find by fs

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,6 +5,6 @@ pacman -S sway swaylock swayidle waybar wofi mako brightnessctl
 # Wayland compatibility
 pacman -S xdg-desktop-portal-wlr xdg-desktop-portal-gtk qt5-wayland xlsclients xorg-xwayland xorg-xlsclients
 # Tools
-pacman -S fish bat btop lsd duf procs fzf zoxide grim flameshot neovim
+pacman -S fish bat btop lsd duf procs fzf fd zoxide grim flameshot neovim
 # Fonts
 pacman -S otf-font-awesome
