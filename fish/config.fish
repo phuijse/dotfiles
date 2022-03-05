c6a79bcbd80bb402dc79df8a68c5ce4273b4c456
Pablo Huijse <pablo.huijse@gmail.com>
2022-03-05 01:59:45 -0300

    get conda on path

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+fish_add_path -p /home/{$USER}/.local/bin 
+source /opt/miniconda3/etc/fish/conf.d/conda.fish
