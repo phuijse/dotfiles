3ecd6d437cbeabcb0eabe89763da509548211e46
Pablo Huijse <pablo.huijse@gmail.com>
2022-03-05 02:06:29 -0300

    Start sway from fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,19 @@ end
 
 fish_add_path -p /home/{$USER}/.local/bin 
 source /opt/miniconda3/etc/fish/conf.d/conda.fish
+
+export XDG_SESSION_TYPE=wayland
+export XDG_CURRENT_DESKTOP=sway
+export XDG_SESSION_DESKTOP=sway
+export XDG_CONFIG_HOME=$HOME/.config
+export MOZ_ENABLE_WAYLAND=1
+export SDL_VIDEODRIVER=wayland
+export _JAVA_AWT_WM_NONREPARENTING=1
+export QT_QPA_PLATFORM=wayland
+
+
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec dbus-run-session sway
+    end
+end
