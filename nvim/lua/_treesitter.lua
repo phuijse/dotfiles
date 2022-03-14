77ef6c528e34d11c483ebdf769cc97bf82dc74d2
Pablo Huijse <pablo.huijse@gmail.com>
2022-03-13 21:32:11 -0300

    Update config and name

--- nvim/lua/_treesitter.lua
+++
@@ -1,9 +0,0 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained",
-  sync_install = false,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-}
-
