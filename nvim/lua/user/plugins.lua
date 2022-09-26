8a8e6c52e9f79d48511a4440c4fc0668cd6794ee
Pablo Huijse <pablo.huijse@gmail.com>
2022-09-26 17:35:35 -0300

    bootstrap packer for first build

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -15,7 +15,7 @@ local packer_bootstrap = ensure_packer()
 vim.cmd([[
   augroup packer_user_config
     autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
   augroup end
 ]])
 
