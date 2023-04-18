462fd63786602bbe323a02741373c9462827057e
Pablo Huijse <pablo.huijse@gmail.com>
2023-04-17 20:38:48 -0400

    new icon configuration

--- nvim/lua/user/barbar.lua
+++ nvim/lua/user/barbar.lua
@@ -24,20 +24,12 @@ require'bufferline'.setup {
   -- Enable/disable icons
   -- if set to 'numbers', will show buffer index in the tabline
   -- if set to 'both', will show buffer index and icons in the tabline
-  icons = true,
-
-  -- If set, the icon color will follow its corresponding buffer
-  -- highlight group. By default, the Buffer*Icon group is linked to the
-  -- Buffer* group (see Highlighting below). Otherwise, it will take its
-  -- default value as defined by devicons.
-  icon_custom_colors = false,
-
-  -- Configure icons on the bufferline.
-  icon_separator_active = '▎',
-  icon_separator_inactive = '▎',
-  icon_close_tab = '',
-  icon_close_tab_modified = '●',
-  icon_pinned = '車',
+  icons = {
+      filetype = { enabled = true },
+      separator = {left = '▎', right = ''},
+      modified = {button = '●'},
+      pinned = {button = '車', filename = true, separator = {right = ''}},
+  },
 
   -- If true, new buffers will be inserted at the start/end of the list.
   -- Default is to insert after current buffer.
